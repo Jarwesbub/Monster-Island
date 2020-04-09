9ade34e3d53caae21d5bc39464d2d2d0b32075c3
LemonSkull <jarwes90@gmail.com>
2020-04-09 13:03:57 +0300

    Testing stuff

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+
+
+    Vector3 movement;
+
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //  EI TOIMI rb.MovePosition(rb.position + movement + moveSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Scripts/NPCcollision.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class NPCcollision : MonoBehaviour
+{
+
+
+    public GameObject FloatingTextPrefab;
+
+    private int Check = 0;
+
+    private bool Talk = false;
+  
+
+    
+    void OnTriggerEnter (Collider other)
+    {
+        Check = 2; //Hello
+
+    }
+    void OnTriggerExit(Collider other)
+    {
+
+        Check = 1; //BYE
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Check = 3; //??
+
+    }
+
+    void ShowFloatingText()
+    {
+        if (FloatingTextPrefab != null)
+        {
+            var text = Instantiate(FloatingTextPrefab, transform.position, Quaternion.identity, transform);
+            text.GetComponent<TextMesh>().text = "Hello!";
+        }       
+    }
+    private IEnumerator TalkingTime()
+    {
+
+        yield return new WaitForSeconds(2f);
+
+
+        //Destroy(FloatingTextPrefab);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (FloatingTextPrefab != null && Check == 2 && Talk == false)
+        {
+            var text = Instantiate(FloatingTextPrefab, transform.position, Quaternion.identity, transform);
+            text.GetComponent<TextMesh>().text = "Hello!";
+            Talk = true;
+
+            //StartCoroutine(TalkingTime());
+            
+            Destroy(text,1);
+            
+              
+            
+       }
+
+
+        if (FloatingTextPrefab != null && Check == 1 && Talk == false)
+        {
+            var text = Instantiate(FloatingTextPrefab, transform.position, Quaternion.identity, transform);
+            text.GetComponent<TextMesh>().text = "Bye!";
+            Talk = true;
+
+            StartCoroutine(TalkingTime());
+
+
+        }
+
+
+
+
+    }
+}
