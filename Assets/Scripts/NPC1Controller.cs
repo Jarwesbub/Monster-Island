2ad89f6e2ccc42a9cad7631430200778e3540aab
LemonSkull <jarwes90@gmail.com>
2020-04-15 13:30:34 +0300

    Dialogue and screen

---
+++ Assets/Scripts/NPC1Controller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC1Controller : MonoBehaviour
+{
+    // Collision trigger for Dialogue
+    public GameObject NPC1;
+    // Full dialogue active or not
+    public GameObject NPC1Dialogue;
+    // Talk button
+    public GameObject NPC1TalkButton;
+    // Dialogue and continue button
+    public GameObject NPC1DialogueBox;
+
+
+    void Start()
+    {
+        NPC1TalkButton.SetActive(false);
+        NPC1DialogueBox.SetActive(false);
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        NPC1TalkButton.SetActive(true);
+        NPC1DialogueBox.SetActive(true);
+
+    }
+    void OnTriggerExit(Collider other)
+    {
+        NPC1TalkButton.SetActive(false);
+        NPC1DialogueBox.SetActive(false);
+
+    }
+
+}
---
+++ Assets/Scripts/TalkButtonController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TalkButtonController : MonoBehaviour
+{
+    public GameObject NPC1TalkButton;
+    public GameObject NPC1DialogueBox;
+
+    public Button yourButton;
+
+
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        //NPC1TalkButton.SetActive(false);
+        NPC1DialogueBox.SetActive(true);
+    }
+
+}
