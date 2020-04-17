fab62cff09be8ddd65761af2a14ae2800d6f1d90
LemonSkull <jarwes90@gmail.com>
2020-04-17 13:25:33 +0300

    Updates

--- Assets/Scripts/ItemDrop.cs
+++ Assets/Scripts/ItemDrop.cs
@@ -5,17 +5,79 @@ using UnityEngine.UI;
 
 public class ItemDrop : MonoBehaviour
 {
-    //public Button myButton;
+    public GameObject WhatItem;
+
+    public GameObject Heart;
+    
+
+    void Start()
+    {
 
 
+    }
 
 
     public void DropItem()
     {
-        
-        Debug.Log("Painoit Nappia");
-        
+        //(GameObject.Find("Player").GetComponent<PlayerController3D>().PlayerColNPC1);
+
+        if ((GameObject.Find("Player").GetComponent<PlayerController3D>().PlayerColNPC1) == true)
+        {
+            //Debug.Log("NPC1");
+
+            if (WhatItem.name == ("FlowerButton"))
+            {
+                
+                Debug.Log("Flower WTF");
+            }
+            
+            if (WhatItem.name == ("StarButton"))
+            {
+                Debug.Log("Star WTF");
+            }
+
+        }
+        if ((GameObject.Find("Player").GetComponent<PlayerController3D>().PlayerColNPC2) == true)
+        {
+            //Debug.Log("NPC2");
+
+            if (WhatItem.name == ("FlowerButton"))
+            {
+
+                Debug.Log("WOW FLOWER THANKS!");
+                Heart.SetActive(true);
+            }
+
+            if (WhatItem.name == ("StarButton"))
+            {
+                Debug.Log("Star WTF");
+            }
+
+        }
+        if ((GameObject.Find("Player").GetComponent<PlayerController3D>().PlayerColNPC3) == true)
+        {
+            //Debug.Log("NPC3");
+
+            if (WhatItem.name == ("FlowerButton"))
+            {
+
+                Debug.Log("Flower WTF");
+            }
+
+            if (WhatItem.name == ("StarButton"))
+            {
+                Debug.Log("Star OMG THANKS");
+                Heart.SetActive(true);
+            }
+
+        }
+
+
     }
+
+        
+
+    
     /*
     
     void OnEnable()
